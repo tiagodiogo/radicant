790dc6608dd4eff23daece68fe06d3cff8579a52
Tiago Diogo <tiago.diogo@ist.utl.pt>
2023-02-19 20:27:12 +0000

    update README.md. add javadocs to the PhoneBookService.

--- README.md
+++ README.md
@@ -23,19 +23,27 @@ Please ignore the majority of the files, what really matters is at:
 Implementation details
 
 - I've created a small model (PhoneBookRecord) to represent the entities being stored in CSV format
-- The database layer implements the provided Interface. Everything up works with the created domain.
+- The database layer implements the provided Interface. Everything up works with the created domain
 - The resource is fully documented and makes use of the proper http verbs and response codes
 - The service acts as a bridge and mapper between the resource and the database
-- A locking mechanism was used to handle concurrent operations.
-- Logging is performed on endpoint entry and on error handling.
+- A locking mechanism was used to handle concurrent operations
+- A try with resources approach was used to ensure the file is closed at the end of the statement
+- Logging is performed across the request with different levels
+
+Given the allotted time some future improvements could be
+
+- Use a CSV library like Apache Commons CSV to simplify the file handling
+- Use streaming techniques if there are large file requirements
+- Add integration tests that validate the HTTP codes and error cases
+- Improve the locking mechanism to introduce fairness to the write access
+- Move the file name/path declaration to the application properties
 
 You may compile the source code into an executable jar and run it by executing:
 
 - ./mvnw package -P prod,api-docs
 - java -jar radicant-0.0.1-SNAPSHOT.jar
-  This will also run the included tests.
 
-Tests need access to /tmp for file creation, modification and deletion.
+This will also run the included tests. Tests need access to /tmp for file creation, modification and deletion.
 
 By navigating to http://localhost:8080/admin/docs you can access the Swagger API
 
--- src/main/java/com/tiagodiogo/radicant/service/PhoneBookService.java
+++ src/main/java/com/tiagodiogo/radicant/service/PhoneBookService.java
@@ -8,9 +8,14 @@ import java.util.stream.Collectors;
 import java.util.stream.Stream;
 import org.springframework.stereotype.Service;
 
+/**
+ * Service to perform CRUD operations for {@link PhoneBookRecord} contained on a CSV file database.
+ * Also handles conversions from CSV strings to {@link PhoneBookRecord} entities and the other way around.
+ */
 @Service
 public class PhoneBookService {
 
+    public static final long SELECT_ALL = -1L;
     private static final String CSV_SEPARATOR = ",";
     private final PhoneBookDatabase phoneBookDatabase;
 
@@ -18,28 +23,57 @@ public class PhoneBookService {
         this.phoneBookDatabase = phoneBookDatabase;
     }
 
+    /**
+     * Obtain all {@link PhoneBookRecord} from the CSV file database.
+     * @return the List of {@link PhoneBookRecord}.
+     */
     public List<PhoneBookRecord> getAllRecords() {
-        List<String> rawRecords = phoneBookDatabase.select(-1L);
+        List<String> rawRecords = phoneBookDatabase.select(SELECT_ALL);
         return processRawRecords(rawRecords).collect(Collectors.toList());
     }
 
+    /**
+     * Fetch a {@link PhoneBookRecord} by its identifier from the CSV file database.
+     * @param id the record identifier.
+     * @return an Optional with the record, if it exists, or an empty optional if no record was found for that identifier.
+     */
     public Optional<PhoneBookRecord> getRecordById(Long id) {
         List<String> rawRecords = phoneBookDatabase.select(id);
         return processRawRecords(rawRecords).findFirst();
     }
 
+    /**
+     * Persist a new {@link PhoneBookRecord} into the CSV file database.
+     * @param phoneBookRecord the new record to be persisted.
+     * @return the new record identifier.
+     */
     public Long addRecord(PhoneBookRecord phoneBookRecord) {
         return phoneBookDatabase.insert(phoneBookRecord.toCSV());
     }
 
+    /**
+     * Updates an existing {@link PhoneBookRecord} by supplying a new entity to override.
+     * @param phoneBookRecord the entity holding de field to override.
+     * @return true if the record was updated, false otherwise.
+     */
     public boolean updateRecord(PhoneBookRecord phoneBookRecord) {
         return phoneBookDatabase.update(phoneBookRecord.getId(), phoneBookRecord.toCSV());
     }
 
+    /**
+     * Deletes an existing {@link PhoneBookRecord} by its identifier.
+     * @param id the identifier of the record to delete.
+     * @return true if the record was deleted, false otherwise.
+     */
     public boolean deleteRecord(Long id) {
         return phoneBookDatabase.delete(id);
     }
 
+    /**
+     * Converts a List of raw String records into a stream of PhoneBookRecords.
+     * @param rawRecords a list of comma separated strings that represent a raw phone book record.
+     * @return a Stream of {@link PhoneBookRecord} obtained from the @rawRecords.
+     */
     private Stream<PhoneBookRecord> processRawRecords(List<String> rawRecords) {
         return rawRecords.stream().map(s -> s.split(CSV_SEPARATOR)).map(PhoneBookRecord::new);
     }
