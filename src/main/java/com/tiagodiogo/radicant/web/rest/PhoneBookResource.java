21bd2af229cd4187846aa15436ad4d7d19795509
Tiago Diogo <tiago.diogo@ist.utl.pt>
2023-02-19 03:01:24 +0000

    add javadocs to the PhoneBookResource

--- src/main/java/com/tiagodiogo/radicant/web/rest/PhoneBookResource.java
+++ src/main/java/com/tiagodiogo/radicant/web/rest/PhoneBookResource.java
@@ -31,18 +31,37 @@ public class PhoneBookResource {
         this.phoneBookService = phoneBookService;
     }
 
+    /**
+     * {@code GET /phone-book} : Obtain all the phone book records.
+     *
+     * @return the {@link ResponseEntity} with status {@code 200(OK)} and in the body a List of {@link PhoneBookRecord}
+     */
     @GetMapping
     public ResponseEntity<List<PhoneBookRecord>> getPhoneRecords() {
         log.info("REST request to get all phone records");
         return ResponseEntity.ok().body(phoneBookService.getAllRecords());
     }
 
+    /**
+     * {@code GET /phone-book/{recordId}} : Fetch a single phone book record by its identifier.
+     *
+     * @param recordId the phone book record identifier.
+     * @return the {@link ResponseEntity} with status {@code 200(OK)} and in the body the {@link PhoneBookRecord}.
+     * Or, a {@link ResponseEntity} with status {@code 404(NOT_FOUND)} if no {@link PhoneBookRecord} matched the provided identifier.
+     */
     @GetMapping("/{recordId}")
     public ResponseEntity<PhoneBookRecord> getPhoneRecordById(@PathVariable Long recordId) {
         log.info("REST request to get phone record by id: {}", recordId);
         return ResponseUtil.wrapOrNotFound(phoneBookService.getRecordById(recordId));
     }
 
+    /**
+     * {@code POST /phone-book} : Create a new phone book record.
+     *
+     * @param phoneBookRecord the record to persist.
+     * @return the {@link ResponseEntity} with status {@code 201(CREATED)} and in the body the persisted {@link PhoneBookRecord}.
+     * @throws URISyntaxException in the event of a badly formed URI.
+     */
     @PostMapping
     public ResponseEntity<Long> createPhoneRecord(@RequestBody PhoneBookRecord phoneBookRecord) throws URISyntaxException {
         log.info("REST request to create a new phone record defined by: {}", phoneBookRecord);
@@ -50,6 +69,13 @@ public class PhoneBookResource {
         return ResponseEntity.created(new URI("api/phone-book/" + newRecordId)).body(newRecordId);
     }
 
+    /**
+     * {@code PUT /phone-book} : Update an existing phone book record.
+     *
+     * @param phoneBookRecord the record to update.
+     * @return the {@link ResponseEntity} with status {@code 204(NO_CONTENT)}.
+     * Or, a {@link ResponseStatusException} with status {@code 404(NOT_FOUND)} if no {@link PhoneBookRecord} matched the provided identifier.
+     */
     @PutMapping
     public ResponseEntity<Void> updatePhoneRecord(@RequestBody PhoneBookRecord phoneBookRecord) {
         log.info("REST request to update an existing phone record with id: {}", phoneBookRecord.getId());
@@ -58,6 +84,13 @@ public class PhoneBookResource {
         } else throw new ResponseStatusException(HttpStatus.NOT_FOUND);
     }
 
+    /**
+     *  {@code DELETE /phone-book/{recordId}} : Delete an existing phone book record.
+     *
+     * @param recordId the phone book record identifier
+     * @return the {@link ResponseEntity} with status {@code 204(NO_CONTENT)}.
+     * Or, a {@link ResponseStatusException} with status {@code 404(NOT_FOUND)} if no {@link PhoneBookRecord} matched the provided identifier.
+     */
     @DeleteMapping("/{recordId}")
     public ResponseEntity<Void> deletePhoneRecord(@PathVariable Long recordId) {
         log.info("REST request to delete an existing phone record with id: {}", recordId);
